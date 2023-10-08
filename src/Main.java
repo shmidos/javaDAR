d17b8473335f4f8ec931b9b9e3c2c171100fe153
Artur <shmidt308@yandex.kz>
2023-10-08 14:21:14 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,75 @@
 public class Main {
     public static void main(String[] args) {
-        String flowersName = "Roses";
-        int flowersNumber = 101;
-        System.out.println(flowersName+" "+flowersNumber);
 
-        int a = 15;
-        int b = 10;
-        System.out.println(a+b);
-        System.out.println(a+"+"+b+"=" +(a+b));
+        // 4.1
+        System.out.println("4.1 Напишите программу для умножения 10 на 5 и вывода результата в консоль.");
+        int a = 10;
+        int b = 5;
+        System.out.println(a + "*" + b + "=" + (a * b));
 
-        int c = a+b;
-        System.out.println(a+ "+"+b+"="+c);
+        // 4.2
+        System.out.println("4.2 Напишите программу для деления 20 на 2.5 и вывода результата в консоль.");
+        double x = 2.5;
+        double y = 20;
+        System.out.println(y + "/" + x + "=" + (y / x));
 
-        a=55; b=65; c=75;
-        System.out.println(a+b+c);
-        System.out.println(a +"+" + b +"+" + c + "=" + (a+b+c));
+        // 4.3
+        System.out.println("4.3 Длина и ширина прямоугольника равны 15 и 9 соответственно. Напишите программу для " +
+                "вычисления площади и периметра прямоугольника.");
+        int l = 15;
+        int w = 9;
+        int s = l * w;
+        System.out.println("Результат: " + l + "*" + w + "=" + s);
 
-        int t,y,u,i;
-        t = y =u = i = 100;
-        System.out.println(t+y+u+i);
-        System.out.println(t +"+" + y +"+" + u + "+" + i + "=" + (t+y+u+i));
+        // 4.4
+        System.out.println("4.4 Используйте правильный оператор, чтобы увеличить значение переменной x на 1 с помощью " +
+                "префиксной и постфиксной форм. Выведите на экран результат.");
+        x = 10;
+        System.out.println(x++);
 
-        double w;
-        w=15;
-        System.out.println(w);
+        // 4.5
+        System.out.println("4.5 Используйте оператор присваивания сложения, чтобы добавить значение 5 к переменной x.");
+        int e = 10;
+        System.out.println("e+=5: " + (e += 5));
 
-        final double PI = 3.14159;
-        System.out.println("Число PI = " + PI);
-        final double GrAc= 9.8;
-        // GrAc = gravitational acceleration
-        System.out.println("Ускорение свободного падения = " + GrAc);
-        final String MyHome = "Алматы";
-        System.out.println("Мой родной город " + MyHome);
+        // 4.6
+        System.out.println("4.6 Напишите программу для проверки равенства двух чисел 25 и 43");
+        int v = 25;
+        int n = 43;
+        System.out.println(v + "==" + n + ": " + (v == b));
 
-        byte byteVar = 123;
-        short shortVar = 12345;
-        int intVar = 123456789;
-        long longVar = 324324234345546546L;
-        double doubleVar = 3.14159;
-        float floatVar = 2.71828f;
-        boolean booleanVar = true;
-        char charVar = 'A';
-        String stringVar = "Hello, World!";
-        System.out.println(byteVar);
-        System.out.println(shortVar);
-        System.out.println(intVar);
-        System.out.println(longVar);
-        System.out.println(doubleVar);
-        System.out.println(floatVar);
-        System.out.println(booleanVar);
-        System.out.println(charVar);
-        System.out.println(stringVar);
+        //4.7
+        System.out.println("4.7 Даны две переменные a и b, значения которых равны 10 и 5 соответственно. " +
+                "Напишите программу для сложения, вычитания, умножения, деления, деления по модулю двух переменных");
+        a = 10;
+        b = 5;
+        System.out.println("Сложение (a + b): " + (a + b));
+        System.out.println("Вычитание (a - b): " + (a - b));
+        System.out.println("Умножение (a * b): " + (a * b));
+        System.out.println("Деление (a / b): " + (a / b));
+        System.out.println("Деление по модулю (a % b):" + (a % b));
+
+        //4.8
+        System.out.println("Напишите программу для преобразования градусов Фаренгейта в градусы Цельсия.");
+        double fah = 68;
+        double cel = (fah - 32) * 5 / 9;
+        System.out.println("(" + fah + "F -32) * 5 / 9 = " + cel);
+
+        //4.9
+        System.out.println("Напишите программу, которая прибавляет 10 к числу 451, а затем делит его на 3. " +
+                "Теперь к модулю результата прибавляется 5, а затем полученное значение умножается на 5. " +
+                "Выведите на экран окончательный результат.");
+        int s1 = (451 + 10) / 3;
+        int s2 = (s1 + 5) * 5;
+        System.out.println("Результат: " + s2);
+
+        //4.10
+        System.out.println("Даны две переменные a и b, значения которых равны 65 и 80 соответственно. " +
+                "Напишите программу, чтобы проверить, верны ли оба условия 'a < 50' и 'a < b'.");
+        a = 65;
+        b = 80;
+        boolean a1 = (a < 50) && (a < b);
+        System.out.println(a1);
+
     }
 }
